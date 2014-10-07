625096ef64814e03b40d794509f3dddb51728f8b
kim <kim@example.com>
2014-10-07 16:18:20 +0200

    Initial project version

---
+++ __init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ OpenEQuartersMain
+                                 A QGIS plugin
+ The plugin automates the setup for investigating an area.
+                             -------------------
+        begin                : 2014-10-07
+        copyright            : (C) 2014 by Kim Gülle / UdK-Berlin
+        email                : kimonline@example.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+def classFactory(iface):
+    # load OpenEQuartersMain class from file OpenEQuartersMain
+    from openequartersmain import OpenEQuartersMain
+    return OpenEQuartersMain(iface)

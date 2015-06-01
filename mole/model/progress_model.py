c68b041c36b497dbc7bb4fdc0d8385f9f670abb9
kim <firstdayofjune@users.noreply.github.com>
2015-06-01 14:41:49 +0200

    - Added method to save the current progress to disk

--- mole/model/progress_model.py
+++ mole/model/progress_model.py
@@ -1,6 +1,7 @@
 import io
 import os
 import json
+import shutil
 
 from PyQt4.QtGui import QListView, QStandardItemModel, QStandardItem
 from PyQt4.QtCore import QSize
@@ -15,13 +16,21 @@ class ProgressItemsModel():
         self.load_section_models(config.progress_model)
 
     def load_section_models(self, path):
+        """
+        Open every sectionX.json-file in the given directory and build the QStandardItemModel-Classes accordingly
+        :param path: Direcory which contains the .json-files
+        :type path: str
+        :return:
+        :rtype:
+        """
         try:
             dir_entries = map(lambda entry: os.path.join(path, entry), os.listdir(path))
-            files = filter(lambda entry: os.path.isfile(entry) and entry.endswith('.json'), dir_entries)
-
+            files = filter(lambda entry: os.path.isfile(entry) and os.path.basename(entry).startswith('section') and entry.endswith('.json'), dir_entries)
+            views = []
             for json_section in files:
                 data = io.open(json_section)
                 json_data = json.load(data)
+                data.close()
 
                 step_items = QListView()
                 step_items.setItemDelegate(QProcessViewDelegate(step_items))
@@ -40,12 +49,43 @@ class ProgressItemsModel():
                     section_model.appendRow(item)
 
                 step_items.setModel(section_model)
-                self.section_views.append(step_items)
+                views.append(step_items)
 
-                data.close()
-
+            self.section_views = views
         except IOError, FileNotFoundError:
             print(self.__module__, FileNotFoundError)
+
+
+    def save_section_models(self, path):
+        """
+        Save the current progress to a folder calles 'oeq_progress' under the given path
+        :param path: Directory which will contain the progress
+        :type path: str
+        :return:
+        :rtype:
+        """
+        path = os.path.join(path, 'oeq_progress')
+        try:
+            os.makedirs(path)
+        except OSError, FileExistsError:
+            print(self.__module__, FileExistsError)
+
+
+        default_progress = os.path.join(config.plugin_dir, 'project', 'default_progress')
+        for i in range(1,6):
+            json_file = os.path.join(default_progress, 'section{}.json'.format(i))
+            shutil.copy2(json_file, path)
+            json_file = os.path.join(path, os.path.basename(json_file))
+            model = self.section_views[i-1].model()
+
+            with open(json_file, "r") as jsonFile:
+                json_data = json.load(jsonFile)
+
+            for i, step in enumerate(json_data['steplist']):
+                    step['state'] = model.item(i).checkState()
+
+            with open(json_file, "w") as jsonFile:
+                jsonFile.write(json.dumps(json_data))
 
     def check_prerequisites_for(self, step_name):
         """

6c1785d5f7501e3714bc70c765bd021b696422a7
MaxUdk <Werner.kaul@udk-berlin.de>
2015-06-23 16:48:27 +0200

    Added method to save the WMS-LUT as .qml
    Thus the LUt  taken from WMS can be used as styles in further

--- mole/model/file_manager.py
+++ mole/model/file_manager.py
@@ -79,6 +79,99 @@ class ColorEntryManager():
         del entries[color_entry]
         self.set_color_map_of_layer(entries, layer)
 
+    def write_color_map_as_qml(self, layer_name, out_path):
+
+        def qml_header():
+            return('<!DOCTYPE qgis PUBLIC \'http://mrcc.com/qgis.dtd\' \'SYSTEM\'>\n'
+                    '<qgis version="' + qgis.utils.QGis.QGIS_VERSION + '">\n'
+                    '   <edittypes></edittypes>\n'
+                    '   <renderer-v2 symbollevels="0" type="graduatedSymbol">\n'
+                    '       <ranges>\n')
+        def qml_range(id,lower,upper,label=None):
+            if label == None: label= str(round(float(lower),1)) + ' - ' + str(round(float(upper),1))
+            return '            <range render="true" symbol="'+ str(id) + '" lower="' + str(lower) + '" upper="' + str(upper) + '" label="' + label + '"/>\n'
+
+        def qml_inter1():
+            return('        </ranges>\n'
+                   '        <symbols>\n')
+
+        def qml_symbol(id,R,G,B,alpha):
+            return('            <symbol alpha="1" type="fill" name="' + str(id) + '">\n'
+                    '               <layer pass="0" class="SimpleFill" locked="0">\n'
+                    '                   <prop k="border_width_map_unit_scale" v="0,0"/>\n'
+                    '                   <prop k="color" v="'+ str(R) + ',' + str(G) + ',' + str(B) + ',' + str(alpha) + '"/>\n'
+                    '                   <prop k="joinstyle" v="bevel"/>\n'
+                    '                   <prop k="offset" v="0,0"/>\n'
+                    '                   <prop k="offset_map_unit_scale" v="0,0"/>\n'
+                    '                   <prop k="offset_unit" v="MM"/>\n'
+                    '                   <prop k="outline_color" v="0,0,0,255"/>\n'
+                    '                   <prop k="outline_style" v="solid"/>\n'
+                    '                   <prop k="outline_width" v="0.66"/>\n'
+                    '                   <prop k="outline_width_unit" v="MM"/>\n'
+                    '                   <prop k="style" v="solid"/>\n'
+                    '               </layer>\n'
+                    '           </symbol>\n')
+
+        def qml_inter2():
+            return('        </symbols>\n'
+                    '   </renderer-v2>\n'
+                    '   <customproperties>\n')
+
+        def qml_property(key,value):
+            return('        <property key="'+ str(key) + '" value="' + str(value) + '"/>\n')
+
+        def qml_footer():
+            return('    </customproperties>\n'
+                    '   <blendMode>0</blendMode>\n'
+                    '   <featureBlendMode>0</featureBlendMode>\n'
+                    '   <layerTransparency>0</layerTransparency>\n'
+                    '   <displayfield>gml_id</displayfield>\n'
+                    '   <label>0</label>\n'
+                    '   <labelattributes></labelattributes>\n'
+                    '   <editform>.</editform>\n'
+                    '   <editforminit></editforminit>\n'
+                    '   <featformsuppress>0</featformsuppress>\n'
+                    '   <annotationform>.</annotationform>\n'
+                    '   <editorlayout>generatedlayout</editorlayout>\n'
+                    '   <excludeAttributesWMS/>\n'
+                    '   <excludeAttributesWFS/>\n'
+                    '   <attributeactions/>\n'
+                    '</qgis>')
+
+
+        if self.layer_values_map.has_key(layer_name):
+            out_dict = self.layer_values_map[layer_name]
+            if out_dict:
+                qml=qml_header()
+                cnt=0
+                out_ranges=""
+                out_symbols=""
+                out_properties=""
+                for entry in out_dict.keys():
+                    rgba=entry.replace(" ","").replace("RGBa(","").replace(")","").split(",")
+                    #["Id"]=entry[1]
+                    #["lowLim"]=entry[2]
+                    #["highLim"]=entry[3]
+                    out_ranges=out_ranges + qml_range(cnt,out_dict[entry][1],out_dict[entry][2],None)
+                    print rgba
+                    out_symbols = out_symbols  + qml_symbol(cnt,rgba[0],rgba[1],rgba[2],rgba[3])
+                    if self.layer_abbreviation_map.has_key(layer_name):
+                        out_properties=out_properties + qml_property("LayAbr", self.layer_abbreviation_map[layer_name])
+                    out_properties=out_properties + qml_property("ParName"+ str(cnt), out_dict[entry][0])
+                    cnt+=1
+
+                qml_out= qml_header() + out_ranges + qml_inter1() + out_symbols + qml_inter2() + out_properties + qml_footer()
+
+                try:
+                    with open(out_path, 'w', encoding='utf-8') as qml_file:
+                        qml_file.write(unicode(qml_out))
+
+                    return os.path.exists(out_path)
+                except IOError, Error:
+                    return False
+                    print('{}: {}'.format(self.__module__, Error))
+
+
 
 class MunicipalInformationParser():
     """

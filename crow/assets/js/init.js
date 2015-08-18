5302192e2e864a85257d33e341aba3eaae1da77c
kim <firstdayofjune@users.noreply.github.com>
2015-08-18 15:24:47 +0200

    - A layer-tree is now displayed, every time a layer is added to the layers-collection - The features are highlighted, when the user hovers them - added .brackets-files to .gitignore

---
+++ crow/assets/js/init.js
@@ -0,0 +1,162 @@
+var style = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(255, 255, 255, 0.6)'
+  }),
+  stroke: new ol.style.Stroke({
+    color: '#319FD3',
+    width: 1
+  }),
+  text: new ol.style.Text({
+    font: '12px Calibri,sans-serif',
+    fill: new ol.style.Fill({
+      color: '#000'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#fff',
+      width: 3
+    })
+  })
+});
+var styleCache = [style];
+
+var shapes = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: '../layers/BLD_Shapes.geojson',
+        format: new ol.format.GeoJSON()
+    }),
+    style: function(feature, resolution) {
+        style.getText().setText(resolution < 5000 ? feature.get('name') : '');
+        return styleCache
+    }
+});
+shapes.set('name', 'BLD_Shapes');
+    
+var bldData = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: '../layers/BLD_Data.geojson',
+        format: new ol.format.GeoJSON()
+    }),
+    style: function(feature, resolution) {
+        style.getText().setText(resolution < 5000 ? feature.get('name') : '');
+        return styleCache
+    }
+});
+bldData.set('name', 'BLD_Data');
+
+var invArea = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: '../layers/Investigation Area.geojson',
+        format: new ol.format.GeoJSON()
+    }),
+    style: function(feature, resolution) {
+        style.getText().setText(resolution < 5000 ? feature.get('name') : '');
+        return styleCache
+    }
+});
+invArea.set('name', 'Investigation Area');
+
+var bldCentroids = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: '../layers/BLD_Centroids.geojson',
+        format: new ol.format.GeoJSON()
+    }),
+    style: function(feature, resolution) {
+        style.getText().setText(resolution < 5000 ? feature.get('name') : '');
+        return styleCache
+    }
+});
+bldCentroids.set('name', 'BLD_Centroids');
+
+var openStreetMap = new ol.layer.Tile({
+  source: new ol.source.OSM({projection: 'EPSG:3857'})
+});
+openStreetMap.set('name', 'Open Street Map');
+
+var map = new ol.Map({
+  layers: [openStreetMap],
+  target: 'map',
+  view: new ol.View({
+    center: [1492977,6855322],
+    zoom: 6
+  })
+});
+
+function oeq_init () {
+
+    map.addLayer(shapes);
+    //map.addLayer(invArea);
+    updateLayerList();
+    map.getLayers().on('add', updateLayerList);
+}
+
+
+var highlightStyleCache = {};
+
+var featureOverlay = new ol.layer.Vector({
+  source: new ol.source.Vector(),
+  map: map,
+  style: function(feature, resolution) {
+    var text = resolution < 5000 ? feature.get('name') : '';
+    if (!highlightStyleCache[text]) {
+      highlightStyleCache[text] = [new ol.style.Style({
+        stroke: new ol.style.Stroke({
+          color: '#f00',
+          width: 1
+        }),
+        fill: new ol.style.Fill({
+          color: 'rgba(255,0,0,0.1)'
+        }),
+        text: new ol.style.Text({
+          font: '12px Calibri,sans-serif',
+          text: text,
+          fill: new ol.style.Fill({
+            color: '#000'
+          }),
+          stroke: new ol.style.Stroke({
+            color: '#f00',
+            width: 3
+          })
+        })
+      })];
+    }
+    return highlightStyleCache[text];
+  }
+});
+
+var highlight;
+var displayFeatureInfo = function(pixel) {
+
+  var feature = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
+    return feature;
+  });
+
+    var propertiesSheet = document.getElementById('dataSheet');
+    if (feature) {
+        propertiesSheet.innerHTML = feature.getId() + ': ' + feature.get('name');
+    } else {
+        propertiesSheet.innerHTML = '&nbsp;';
+    }
+
+  if (feature !== highlight) {
+    if (highlight) {
+      featureOverlay.getSource().removeFeature(highlight);
+    }
+    if (feature) {
+      featureOverlay.getSource().addFeature(feature);
+    }
+    highlight = feature;
+  }
+
+};
+
+map.on('pointermove', function(evt) {
+  if (evt.dragging) {
+    return;
+  }
+  var pixel = map.getEventPixel(evt.originalEvent);
+  displayFeatureInfo(pixel);
+});
+
+map.on('click', function(evt) {
+  displayFeatureInfo(evt.pixel);
+});
--- crow/assets/js/oeq-layermenu.js
+++ crow/assets/js/oeq-layermenu.js
@@ -1,77 +1,5 @@
-var style = new ol.style.Style({
-  fill: new ol.style.Fill({
-    color: 'rgba(255, 255, 255, 0.6)'
-  }),
-  stroke: new ol.style.Stroke({
-    color: '#319FD3',
-    width: 1
-  }),
-  text: new ol.style.Text({
-    font: '12px Calibri,sans-serif',
-    fill: new ol.style.Fill({
-      color: '#000'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#fff',
-      width: 3
-    })
-  })
-});
-var styleCache = [style];
-
-var shapes = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: '../layers/BLD_Shapes.geojson',
-        format: new ol.format.GeoJSON()
-    }),
-    style: function(feature, resolution) {
-        style.getText().setText(resolution < 5000 ? feature.get('name') : '');
-        return styleCache
-    }
-});
-
-var bldData = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: '../layers/BLD_Data.geojson',
-        format: new ol.format.GeoJSON()
-    }),
-    style: function(feature, resolution) {
-        style.getText().setText(resolution < 5000 ? feature.get('name') : '');
-        return styleCache
-    }
-});
-
-var invArea = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: '../layers/Investigation Area.geojson',
-        format: new ol.format.GeoJSON()
-    }),
-    style: function(feature, resolution) {
-        style.getText().setText(resolution < 5000 ? feature.get('name') : '');
-        return styleCache
-    }
-});
-
-var map = new ol.Map({
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM({projection: 'EPSG:3857'})
-    }),
-    shapes,
-    bldData,
-    invArea   
-  ],
-  target: 'map',
-  view: new ol.View({
-    center: [1492977,6855322],
-    zoom: 6
-  })
-});
-
-
-
 document.getElementById('add-centroids').onclick = function () {
-    map.addLayer(data);
+    map.addLayer(bldCentroids);
 };
 
 
@@ -92,3 +20,21 @@ document.getElementById('add-wmslayer').onclick = function () {
     map.addLayer(wmsBerlin);
 };
 
+function updateLayerList () {
+    
+    var layers = map.getLayers();
+    var numberOfLayers = layers.getLength();
+    
+    var layersMenu = document.getElementById("layersMenu");
+    layersMenu.innerHTML = "<i class='fa fa-list-alt'></i> Layers (" + numberOfLayers + ")";
+    
+    var layerList = document.getElementById("layerList");
+    finalList = ""
+    
+    for ( var i=0; i < numberOfLayers; i++ ) {
+        var layer = layers.item(i);
+        finalList += "<a href='#' class='list-group-item'><i class='fa fa-globe'></i> " + layer.get('name') + "</a>";
+    }
+    
+    layerList.innerHTML = finalList;
+}

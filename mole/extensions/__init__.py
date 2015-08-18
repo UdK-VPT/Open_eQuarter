febd2ac5ef1ae0a5d5301d6af7daf666663409c0
Maxwell Smart <werner.kaul@udk-berlin.de>
2015-08-18 16:24:29 +0200

    Implemented basic extension handling

--- mole/extensions/__init__.py
+++ mole/extensions/__init__.py
@@ -1,6 +1,5 @@
-import os, glob, sys
+import glob
 from mole.oeq_global import *
-from mole.model.file_manager import InformationSource
 
 
 class oeq_extension:
@@ -21,28 +20,39 @@ class oeq_extension:
         self.type = type
         self.source = source
         if parname_1 is None:
-            self.parname_1 = symbol + '_L'
+            self.parname_1 = field_id + '_L'
         else:
             self.parname_1 = parname_1
         if parname_2 is None:
-            self.parname_2 = symbol + '_H'
+            self.parname_2 = field_id + '_H'
         else:
             self.parname_2 = parname_2
         if parnames_out is None:
-            self.parnames_out = symbol
+            self.parnames_out = field_id
         else:
             self.parnames_out = parnames_out
         self.evaluator = evaluation_method
 
+    def getInformationSource(self):
+        return InformationSource(extension=self.extension_name,
+                                 type=self.type,
+                                 field_id=self.field_id,
+                                 layer_name=self.layer_name,
+                                 source=self.source)
 
-def getInformationSource(self):
-    return InformationSource(extension=self.extension_name, type=self.type, field_id=self.field_id,
-                             layer_name=self.layer_name, source=self.source)
+    def registerExtension(self):
+        print "register extension"
+        OeQ_ImportExtensionRegistry.append(self)
 
+    def registerDialog(self):
+        print "register dialog"
+        OeQ_information_defaults.append(self.getInformationSource())
 
-def evaluate(self, param):
-    self.evaluator(self, param)
+    def evaluate(self):
+        self.evaluator(self)
 
 
 modules = glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
 __all__ = [os.path.basename(f)[:-3] for f in modules if not f.endswith("__init__.py")]
+from . import *
+

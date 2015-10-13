a5b7d95c4bd76dcca1407a54b9ba424912cf97be
kim <firstdayofjune@users.noreply.github.com>
2015-10-13 17:35:53 +0200

    Changed the app to connect to a postgis database instead of the sqlite-db

--- crow/geo_django/geo_django/settings.py
+++ crow/geo_django/geo_django/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'geo_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'crow',
+        'USER': 'geodjango',
     }
 }
 

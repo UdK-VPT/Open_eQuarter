ff3c4472682ae37ce8130831f3241b01e07e40f9
kim <firstdayofjune@users.noreply.github.com>
2015-10-13 13:00:38 +0200

    Changed the FT to use the unittest framework

--- crow/geo_django/functional_test.py
+++ crow/geo_django/functional_test.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_view_an_open_layers_map(self):
+        # The GIS community has heard of a new tool to analyse a quarter.
+        # Mr. G. opens his webbrowser and opens the url.
+        self.browser.get('http://localhost:8000')
+
+        # He expects the title of the tool to be in the browser
+        self.assertIn('Open eQuarter - Crow', self.browser.title)
+        self.fail('Finish the test')
+
+        # A header navigation is displayed, which shows the companies logo
+
+if __name__ == '__main__':
+    unittest.main()

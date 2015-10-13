37323eb57173a6391349dd98cae3764be9465276
kim <firstdayofjune@users.noreply.github.com>
2015-10-13 13:17:03 +0200

    Added an app for the layertree and created a view test for it

--- crow/geo_django/geo_django/urls.py
+++ crow/geo_django/geo_django/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'layertree.views.layer_tree', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
---
+++ crow/geo_django/layertree/tests.py
@@ -0,0 +1,10 @@
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from layertree.views import layer_tree
+
+
+class TreeRootTest(TestCase):
+
+    def test_root_url_resolves_to_layer_tree(self):
+        found = resolve('/')
+        self.assertEqual(found.func, layer_tree)

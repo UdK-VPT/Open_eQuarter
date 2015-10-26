cda017784e1ee7fa8465a6f349062cc335a5b6d8
kim <firstdayofjune@users.noreply.github.com>
2015-10-26 13:07:08 +0100

    Enhanced the functional test

--- crow/crow_django/functional_test.py
+++ crow/crow_django/functional_test.py
@@ -5,7 +5,11 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
+        # Headless test using phantomjs driver
         self.browser = webdriver.PhantomJS('phantomjs')
+
+        # The browsers window-size is set to a desktop resolution
+        self.browser.set_window_size(1024, 768)
 
     def tearDown(self):
         self.browser.quit()
@@ -24,6 +28,20 @@ class NewVisitorTest(unittest.TestCase):
         logo = logo_link.find_element_by_tag_name('img')
         self.assertTrue(logo.get_attribute('src'))
 
+        # A dropdown menu is visible in the navigation bar, where Mr. G can chose a project
+        dropdown = navigation_bar.find_element_by_class_name('dropdown')
+        dropdown_link = dropdown.find_element_by_tag_name('a')
+        self.assertIn('Choose project', dropdown_link.text,
+                      'Choose project - dropdown not found, found {} instead.'.format(dropdown_link.text))
+
+        # Another link is found, which enables the user to load a layer
+        navigation_ul = navigation_bar.find_element_by_css_selector('ul:first-child')
+        open_button_link = navigation_ul.find_element_by_class_name('btn-file')
+        self.assertIn('Open layer', open_button_link.text,
+                      'Open layer - button not found, found {} instead'.format(open_button_link.text))
+
         self.fail('Finish the test')
+
+
 if __name__ == '__main__':
     unittest.main()

840a09e728b311e1645f6617a5258067107d0fd8
kim <firstdayofjune@users.noreply.github.com>
2015-12-04 15:44:56 +0100

    Implemented the registration process completely

--- crow/crow_django/crow_django/settings.py
+++ crow/crow_django/crow_django/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'crow_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates', 'crow_django')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- crow/crow_django/crow_django/urls.py
+++ crow/crow_django/crow_django/urls.py
@@ -20,9 +20,7 @@ from django_jasmine import urls as django_jasmine
 
 urlpatterns = [
     url(r'^$', 'crow.views.home_page', name='home'),
-    url(r'^accounts/login/', 'django.contrib.auth.views.login', name='login', kwargs={'template_name': 'login.html'}),
     url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^jasmine/', include(django_jasmine)),
 ]

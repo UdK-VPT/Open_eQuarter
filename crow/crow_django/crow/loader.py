399c22896d58a4162a4280dbc71226eff3eafdfe
firstdayofjune <firstdayofjune@users.noreply.github.com>
2016-01-15 18:05:35 +0100

    Changed loader to be python3 compliant

--- crow/crow_django/crow/loader.py
+++ crow/crow_django/crow/loader.py
@@ -1,14 +1,14 @@
 import os, sys
 from django.contrib.gis.utils import LayerMapping
-from models import OeQLayer, oeq_layer_mapping
+from crow.models import OeQLayer, oeq_layer_mapping
 
 
-def run(shp_location='../../layers/Heinrich_db.shp', verbose=True):
+def run(shp_location='../../../OeQ_Unters/Heinrich_db.shp', verbose=True):
 
     try:
         shp = os.path.abspath(os.path.join(os.path.dirname(__file__), shp_location))
         lm = LayerMapping(OeQLayer, shp, oeq_layer_mapping, transform=False, encoding='iso-8859-1')
         lm.save(strict=True, verbose=verbose)
-    except IndexError, MissingArgErr:
+    except IndexError as MissingArgErr:
         print('Please provide the relative location of the shapefile as first argument.')
 

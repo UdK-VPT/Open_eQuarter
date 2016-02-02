7515139565a2c6c0676e6bebbb705bb6360b17d1
firstdayofjune <firstdayofjune@users.noreply.github.com>
2016-02-02 17:56:46 +0100

    Made the LayerDetail-view available to registerd users only

--- crow/crow_django/crow/urls.py
+++ crow/crow_django/crow/urls.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from crow import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^layer_(?P<pk>\d+)/$', views.LayerDetail.as_view(), name='layer_detail'),
+    url(r'^layer_(?P<pk>\d+)/$', login_required(views.LayerDetail.as_view()), name='layer_detail'),
 ]

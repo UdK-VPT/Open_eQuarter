ca6515864c982b768030b80dafafa7cdb06566f8
firstdayofjune <firstdayofjune@users.noreply.github.com>
2016-02-02 16:39:40 +0100

    Refactored layer urls to be prefixed by "layer_"

--- crow/crow_django/crow/urls.py
+++ crow/crow_django/crow/urls.py
@@ -3,5 +3,5 @@ from crow import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^(?P<pk>\d+)/$', views.LayerDetail.as_view(), name='layer_detail'),
+    url(r'^layer_(?P<pk>\d+)/$', views.LayerDetail.as_view(), name='layer_detail'),
 ]

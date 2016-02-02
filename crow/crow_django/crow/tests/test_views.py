4efc00a0816106cd777f502d97657a00bc22eb80
firstdayofjune <firstdayofjune@users.noreply.github.com>
2016-02-02 17:52:48 +0100

    First working comment-solution, which lets a registered user comment on a layer (from the layers detail page)

--- crow/crow_django/crow/tests/test_views.py
+++ crow/crow_django/crow/tests/test_views.py
@@ -5,7 +5,7 @@ from django_webtest import WebTest
 
 from crow.views import home_page
 from crow.models import Layer
-
+from crow.forms import EMPTY_COMMENT_ERROR
 
 class HomePageTest(TestCase):
 
@@ -14,7 +14,7 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
 
-class LayerViewTest(TestCase):
+class LayerViewTest(WebTest):
 
     def setUp(self):
         self.layer = Layer()
@@ -30,3 +30,18 @@ class LayerViewTest(TestCase):
     def test_name_in_layer(self):
         response = self.client.get(self.layer.get_absolute_url())
         self.assertContains(response, self.layer.name)
+
+    def test_view_page(self):
+        page = self.app.get(self.layer.get_absolute_url())
+        self.assertEqual(len(page.forms), 1)
+
+    def test_form_error(self):
+        page = self.app.get(self.layer.get_absolute_url())
+        redirect = page.form.submit()
+        self.assertContains(redirect, EMPTY_COMMENT_ERROR)
+
+    def test_form_success(self):
+        page = self.app.get(self.layer.get_absolute_url(), user=self.user)
+        page.form['text'] = 'This is a comment'
+        redirect = page.form.submit()
+        self.assertRedirects(redirect, self.layer.get_absolute_url())
--- crow/crow_django/crow/views.py
+++ crow/crow_django/crow/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.views.generic import DetailView
+from django.views.generic import CreateView
 from django.shortcuts import render
 
 from crow.models import Layer
+from crow.forms import CommentForm
 
 # Create your views here.
 def login_register(request):
@@ -14,5 +15,24 @@ def home_page(request):
     return render(request, 'crow/index.html', {'layers': Layer.objects.all()})
 
 
-class LayerDetail(DetailView):
+# Define the view which will be used to comment a layer
+class LayerDetail(CreateView):
     model = Layer
+    template_name = 'crow/layer_detail.html'
+    form_class = CommentForm
+
+    # Define additional meta data, which is needed to create a new Comment-Object
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['author'] = self.request.user
+        kwargs['layer'] = self.get_object()
+        return kwargs
+
+    def get_success_url(self):
+        return self.get_object().get_absolute_url()
+
+    # Define the context which will be used when rendering the template
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['layer'] = self.get_object()
+        return data

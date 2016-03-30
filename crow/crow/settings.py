88c0d176cc4735f3e59d802b8a05fa767a6cfa13
firstdayofjune <firstdayofjune@users.noreply.github.com>
2016-03-30 14:47:44 +0200

    Changed DB-backend to use postgresql and ran first migration

--- crow/crow/settings.py
+++ crow/crow/settings.py
@@ -88,9 +88,11 @@ WSGI_APPLICATION = 'crow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'crow',
+        'USER': 'djangocrow',
+        'PASSWORD': 'djangocrow'
+    },
 }
 
 
